e7d93ffaf51fc2bab15fbf3eb66d2f429d6b2ad4
dali chand <k.dcmaida23@gmail.com>
2019-08-29 19:00:08 +0530

    detail page

--- lib/screens/GridItemDetails.dart
+++ lib/screens/GridItemDetails.dart
@@ -1,15 +1,105 @@
 import 'package:flutter/material.dart';
 import 'package:movie_gridview/model/Item.dart';
+import 'package:movie_gridview/screens/GetRatings.dart';
 
-class GridItemDetails extends StatelessWidget{
-
+class GridItemDetails extends StatelessWidget {
   final Item item;
 
   GridItemDetails(this.item);
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      primary: true,
+      appBar: AppBar(
+        title: Text(item.name),
+      ),
+      backgroundColor: Color(0xFF761322),
+      body: ListView(
+        children: <Widget>[
+          HeaderBanner(this.item),
+        ],
+      ),
+    );
   }
+}
 
+class HeaderBanner extends StatelessWidget {
+  final Item item;
+
+  HeaderBanner(this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 0.0,
+      child: Container(
+        height: 380.0,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            HeaderImage(this.item.bannerUrl),
+            HeaderContent(this.item),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HeaderImage extends StatelessWidget {
+  final String bannerUrl;
+
+  HeaderImage(this.bannerUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(bannerUrl,
+        width: 600.0, height: 380.0, fit: BoxFit.cover);
+  }
+}
+
+class HeaderContent extends StatelessWidget {
+  final Item item;
+
+  HeaderContent(this.item);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        constraints: BoxConstraints.expand(height: 110.0),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          child: Container(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 1.0),
+                        child: Text(
+                          item.name,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: TextStyle(
+                              fontSize: 26.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      GetRatings(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
